13ee5d3ea70a01763078d5dde25d818e662f9874
niyolu <nijolu@live.de>
2023-06-25 12:28:31 +0200

    split parse actors into two  functions

--- app/scraping.py
+++ app/scraping.py
@@ -1,20 +1,25 @@
 import asyncio
 import aiohttp
+from aiocache import Cache, cached
+
 import bs4 as bs
 from urllib.parse import urljoin
+
 from datetime import date
 import time
 import typing
-import collections
 
-MAX_ACTORS = 100 # FIXME maybe like 20
+
+MAX_ACTORS = 5 # FIXME maybe like 20
 MAX_MOVIES = 250
+SCRAPE_DELAY = (10, 15)
 
 url_base = "https://www.imdb.com/"
 url_charts = urljoin(url_base, "chart/top")
 
 headers = {
-    "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+    #"user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
     "accept-language": "de-de"
 }
 
@@ -54,30 +59,45 @@ async def parse_movie(
         "rating": rating
     }
 
-# cache -> split up in parse actor
-async def parse_actors(
-        session: aiohttp.client.ClientSession, actors
-    ) -> tuple[list[str], list[int]]:
-    names, ages = [], []
-    for actor in actors:
-        names.append(actor.text)
-        actor_url = urljoin(url_base, actor["href"])
+# actors_parsed = 0
 
-        async with session.get(actor_url) as actor_response:
-            soup = bs.BeautifulSoup(await actor_response.text(), "html.parser")
-            try:
-                date_of_birth = soup.find("li", {"data-testid": "nm_pd_bl"}).find("a").text
-                d, m, y = date_of_birth.split()
-                d = int(d.strip("."))
-                y = int(y)
-                m = get_month_from_ger_str(m)
-            except (AttributeError, ValueError) as e:
-                print("actor without (correctly) specified date of birth or bio: ", names[-1], actor_url, e)
-                age = -1
-            else:
-                age = (date.today() - date(y, m, d)).days // 365
-            ages.append(age)
-    return names, ages
+@cached(key_builder=lambda func, *args, **kwargs: kwargs["actor"].text)
+async def parse_actor(
+        *,
+        session: aiohttp.client.ClientSession,
+        actor: bs.element.Tag
+    ) -> tuple:
+    name = actor.text
+    actor_url = urljoin(url_base, actor["href"])
+    async with session.get(actor_url) as actor_response:
+        soup = bs.BeautifulSoup(await actor_response.text(), "html.parser")
+        try:
+            date_of_birth = soup.find("li", {"data-testid": "nm_pd_bl"}).find("a").text
+            d, m, y = date_of_birth.split()
+            d = int(d.strip("."))
+            y = int(y)
+            m = get_month_from_ger_str(m)
+        except (AttributeError, ValueError) as e:
+            print("actor without (correctly) specified date of birth or bio: ", name, actor_url, e)
+            age = -1
+        else:
+            age = (date.today() - date(y, m, d)).days // 365
+    # global actors_parsed
+    # actors_parsed += 1
+    return name, age
+
+
+async def parse_actors(
+        session: aiohttp.client.ClientSession,
+        actors: bs.element.ResultSet
+    ) -> tuple[list[str], list[int]]:
+    tasks = []
+    for idx, actor in enumerate(actors):
+            if idx >= MAX_ACTORS:
+                break
+            tasks.append(parse_actor(session=session, actor=actor))
+    names_ages = await asyncio.gather(*tasks)
+    return zip(*names_ages)
 
 
 async def parse_top_movies(n: int = MAX_MOVIES) -> list:
@@ -86,10 +106,12 @@ async def parse_top_movies(n: int = MAX_MOVIES) -> list:
         movies = await get_top_movies(session)
         tasks = []
         for idx, movie in enumerate(movies):
-            if n == idx:
+            if idx >= n:
                 break
             tasks.append(parse_movie(session, movie))
+        print(len(tasks))
         movies_info = await asyncio.gather(*tasks)
+    #print(f"{actors_parsed=}")
     return movies_info
 
 
@@ -105,18 +127,17 @@ def run_scraper(
 def test():
     import sys
     import pickle
-    n = sys.argv[1] if len(sys.argv) > 1 else 3#250
-    print(n)
+    n = int(sys.argv[1]) if len(sys.argv) > 1 else 3#250
     start = time.perf_counter()
     movies = run_scraper(parse_top_movies, n)
     print(movies)
-    print(f"script took {time.perf_counter()-start:.2f}s to run")
-    with open(f"top{n}.pkl", "wb") as f: 
-        pickle.dump(movies, f)
-    print(len(movies))
-    
+    print(f"script took {time.perf_counter()-start:.2f}s to parse {len(movies)} movies")
+    if movies:
+        if len(movies) == n or input("fetched {n}/{len(movies)} movies, continue? y/n").lower() == "y":
+            with open(f"top{n}.pkl", "wb") as f: 
+                pickle.dump(movies, f)
 
 
 if __name__ == "__main__":
     test()
-
+    
