211bb91df4db11698b954e3655991eaa46448332
niyolu <nijolu@live.de>
2023-06-25 04:23:42 +0200

    found out how to do multiple batches in a single file

---
+++ init.sql
@@ -0,0 +1,113 @@
+CREATE TABLE movies (
+  id INT IDENTITY(1,1)  PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  year INT NOT NULL,
+  genre VARCHAR(50) NOT NULL,
+  rating DECIMAL(3, 1) NOT NULL
+);
+
+
+CREATE TABLE actors (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age int NOT NULL
+);
+
+
+CREATE TABLE actor_deleted_ratings (
+    actor_id INT,
+    rating DECIMAL(3, 1) NOT NULL
+);
+
+
+CREATE TABLE movie_actors (
+  movie_id INT,
+  actor_id INT,
+  FOREIGN KEY (movie_id) REFERENCES movies(id),
+  FOREIGN KEY (actor_id) REFERENCES actors(id)
+);
+
+
+CREATE FUNCTION split_actor_ages (
+  @actor_ages VARCHAR(MAX)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+  SELECT [key], value AS age
+  FROM STRING_SPLIT(@actor_ages, ',')
+);
+
+CREATE PROCEDURE insert_movie
+  @title VARCHAR(50),
+  @year INT,
+  @genre VARCHAR(50),
+  @actor_names VARCHAR(MAX),
+  @actor_ages VARCHAR(MAX),
+  @rating DECIMAL(3, 1)
+AS
+BEGIN
+  DECLARE @movie_id INT;
+  IF NOT EXISTS (SELECT * FROM movies WHERE title = @title AND year = @year)
+  BEGIN
+    INSERT INTO movies (title, year, genre, rating)
+    VALUES (@title, @year, @genre, @rating);
+    SELECT @movie_id = SCOPE_IDENTITY();
+    DECLARE @actor_id INT;
+    DECLARE @actor_name VARCHAR(50);
+    DECLARE @actor_age INT;
+    DECLARE actor_cursor CURSOR FOR
+      SELECT value, ROW_NUMBER() OVER (ORDER BY (SELECT NULL))
+      FROM STRING_SPLIT(@actor_names, ',');
+    DECLARE @actor_ages_table TABLE (
+      [key] INT PRIMARY KEY,
+      age INT
+    );
+
+    INSERT INTO @actor_ages_table
+    SELECT [key], age
+    FROM split_actor_ages(@actor_ages);
+
+    OPEN actor_cursor;
+    FETCH NEXT FROM actor_cursor INTO @actor_name;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+      SELECT @actor_age = age FROM @actor_ages_table WHERE [key] = @@CURSOR_ROWS;
+
+      IF NOT EXISTS (SELECT * FROM actors WHERE name = @actor_name AND age = @actor_age)
+      BEGIN
+        INSERT INTO actors (name, age)
+        VALUES (@actor_name, @actor_age);
+        SELECT @actor_id = SCOPE_IDENTITY();
+      END
+      ELSE
+      BEGIN
+        SELECT @actor_id = id FROM actors WHERE name = @actor_name AND age = @actor_age;
+      END
+
+      INSERT INTO movie_actors (movie_id, actor_id)
+      VALUES (@movie_id, @actor_id);
+
+      FETCH NEXT FROM actor_cursor INTO @actor_name;
+    END
+
+    CLOSE actor_cursor;
+    DEALLOCATE actor_cursor;
+  END;
+END;
+
+
+CREATE FUNCTION get_actors_for_movie
+  (@movie_id INT)
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+  DECLARE @actor_list VARCHAR(MAX);
+  SELECT @actor_list = STRING_AGG(name, ', ')
+  FROM actors
+  JOIN movie_actors ON actors.id = movie_actors.actor_id
+  WHERE movie_actors.movie_id = @movie_id;
+  RETURN @actor_list;
+END;
