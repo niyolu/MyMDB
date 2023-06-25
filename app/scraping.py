80fb68cf9f87a72139d5a55de90ab8108ffe4eb7
niyolu <nijolu@live.de>
2023-06-25 08:40:02 +0200

    turned scraper async

--- app/scraping.py
+++ app/scraping.py
@@ -1,50 +1,50 @@
+import asyncio
+import aiohttp
 import bs4 as bs
-import requests
 from urllib.parse import urljoin
 from datetime import date
 import time
+import typing
+import collections
 
-MAX_ACTORS = 15
-MAX_MOVIES = 20
-
-s = requests.Session()
-s.headers.update({
-    "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
-    "accept-language": "de-de"
-})
+MAX_ACTORS = 100 # FIXME maybe like 20
+MAX_MOVIES = 250
 
 url_base = "https://www.imdb.com/"
 url_charts = urljoin(url_base, "chart/top")
 
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "accept-language": "de-de"
+}
 
-def get_month_from_ger_str(m_str):
+
+def get_month_from_ger_str(m_str: str):
     return 1 + "Januar,Februar,März,April,Mai,Juni,Juli,August,September,Oktober,November,Dezember".split(",").index(m_str)
 
 
-def get_top_movies() -> bs.element.ResultSet:
-    soup = bs.BeautifulSoup(s.get(url_charts).text, "html.parser")
-    movies = soup.select('td.titleColumn')
-    return movies
+async def get_top_movies(session: aiohttp.client.ClientSession) -> bs.element.ResultSet:
+    async with session.get(url_charts) as response:
+        soup = bs.BeautifulSoup(await response.text(), "html.parser")
+        movies = soup.select('td.titleColumn')
+        return movies
 
-def parse_movie(movie: bs.element.Tag):
+
+async def parse_movie(
+        session: aiohttp.client.ClientSession, 
+        movie: bs.element.Tag
+    ) -> dict:
     title = movie.find("a").text
-    year = int(
-        movie
-        .find("span", {"class": "secondaryInfo"})
-        .text.strip("()")
-    )
-    rating = float(
-        movie
-        .find_next_sibling()
-        .find("strong")
-        .text.replace(",", ".")
-    )
+    year = int(movie.find("span", {"class": "secondaryInfo"}).text.strip("()"))
+    rating = float(movie.find_next_sibling().find("strong").text.replace(",", "."))
     movie_url = urljoin(url_base, movie.find("a")["href"])
-    movie_response = s.get(movie_url)
-    soup = bs.BeautifulSoup(movie_response.text, "html.parser")
-    genre = soup.find("a", class_="ipc-chip ipc-chip--on-baseAlt").text
-    actors = soup.find_all("a", {"data-testid": "title-cast-item__actor"})
-    actor_names, actor_ages = parse_actors(actors)
+
+    async with session.get(movie_url) as movie_response:
+        soup = bs.BeautifulSoup(await movie_response.text(), "html.parser")
+        genre = soup.find("a", class_="ipc-chip ipc-chip--on-baseAlt").text
+        actors = soup.find_all("a", {"data-testid": "title-cast-item__actor"})
+        actor_names, actor_ages = await parse_actors(session, actors)
+
     return {
         "title": title,
         "year": year,
@@ -53,44 +53,70 @@ def parse_movie(movie: bs.element.Tag):
         "actor_ages": actor_ages,
         "rating": rating
     }
-    
 
-def parse_actors(actors: bs.element.ResultSet):
+# cache -> split up in parse actor
+async def parse_actors(
+        session: aiohttp.client.ClientSession, actors
+    ) -> tuple[list[str], list[int]]:
     names, ages = [], []
     for actor in actors:
         names.append(actor.text)
         actor_url = urljoin(url_base, actor["href"])
-        soup = bs.BeautifulSoup(s.get(actor_url).text, "html.parser")
-        try:
-            year_of_birth = soup.find("li", {"data-testid":"nm_pd_bl"}).find("a").text
-            d,m,y = year_of_birth.split()
-            d = int(d.strip("."))
-            y = int(y)
-            m = get_month_from_ger_str(m)
-        except Exception as e:
-            ages.append(-1)
-            print(e, names[-1], actor_url)
-            continue
-        age = (date.today() - date(y, m, d)).days // 365
-        ages.append(age)
+
+        async with session.get(actor_url) as actor_response:
+            soup = bs.BeautifulSoup(await actor_response.text(), "html.parser")
+            try:
+                date_of_birth = soup.find("li", {"data-testid": "nm_pd_bl"}).find("a").text
+                d, m, y = date_of_birth.split()
+                d = int(d.strip("."))
+                y = int(y)
+                m = get_month_from_ger_str(m)
+            except (AttributeError, ValueError) as e:
+                print("actor without (correctly) specified date of birth or bio: ", names[-1], actor_url, e)
+                age = -1
+            else:
+                age = (date.today() - date(y, m, d)).days // 365
+            ages.append(age)
     return names, ages
-        
-    
-def parse_top_movies(n: int = MAX_MOVIES) -> list[dict]:
+
+
+async def parse_top_movies(n: int = MAX_MOVIES) -> list:
     movies_info = []
-    movies = get_top_movies()
-    for idx, movie in enumerate(movies):
-        if n == idx:
-            break
-        time.sleep(.5)
-        movies_info.append(parse_movie(movie))
+    async with aiohttp.ClientSession(headers=headers) as session:
+        movies = await get_top_movies(session)
+        tasks = []
+        for idx, movie in enumerate(movies):
+            if n == idx:
+                break
+            tasks.append(parse_movie(session, movie))
+        movies_info = await asyncio.gather(*tasks)
     return movies_info
 
-def test():
-    print(parse_top_movies(10))
+
+def run_scraper(
+        scraper: typing.Callable[..., typing.Coroutine[typing.Any, typing.Any, typing.Any]],
+        *args, **kwargs
+    ) -> list:
+    loop = asyncio.get_event_loop()
+    movies = loop.run_until_complete(scraper(*args, **kwargs))
+    return movies
     
-        
-if __name__ == "__main__":
+    
+def test():
+    import sys
+    import pickle
+    n = sys.argv[1] if len(sys.argv) > 1 else 3#250
+    print(n)
     start = time.perf_counter()
-    test()
+    movies = run_scraper(parse_top_movies, n)
+    print(movies)
     print(f"script took {time.perf_counter()-start:.2f}s to run")
+    with open(f"top{n}.pkl", "wb") as f: 
+        pickle.dump(movies, f)
+    print(len(movies))
+    
+
+
+if __name__ == "__main__":
+    test()
+
