d7524e476cfa04cbd3f7c3f917d8e5ef9a11d223
niyolu <nijolu@live.de>
2023-06-25 21:28:21 +0200

    improved test, commit, fixed convert

--- app/db.py
+++ app/db.py
@@ -4,7 +4,8 @@ from typing import List
 # for driver in pyodbc.drivers():
 #     print(driver)
 
-# driver = "ODBC Driver 17 for SQL Server"
+
+
 driver = "SQL Server"
 server = "localhost"
 # server = "127.0.0.1"
@@ -29,23 +30,22 @@ cnxn = pyodbc.connect(conn_str)
 cursor = cnxn.cursor()
 
 def insert_movie(
-    title: str,
-    year: int,
-    genre: str,
-    actor_names: list[str],
-    actor_ages: list[int],
-    rating: int
+        title: str,
+        year: int,
+        genre: str,
+        actor_names: list[str],
+        actor_ages: list[int],
+        rating: int
     ):
     def convert(x):
         if isinstance(x, str):
-            return repr(x)
+            return repr(x.replace("'", "§")).replace("§", "''")
         if isinstance(x, (int, float)):
             return str(x)
-        if isinstance(x, list):
+        if isinstance(x, (tuple, list)):
             return convert(",".join(map(str, x)))
         else:
-            print(x, type(x))
-            raise NotImplementedError()
+            raise NotImplementedError(f"no impl for type {type(x)}")
     
     args = ",".join([
         convert(arg) for arg in (
@@ -59,7 +59,10 @@ def insert_movie(
     ])
     call_str = f"{{call insert_movie({args})}}"
     print(call_str)
-    cursor.execute(call_str)
+    res = cursor.execute(call_str)
+    cnxn.commit()
+    return res
+    
     
 def select_all(tablename: str):
     cursor.execute(f"SELECT * from {tablename}")
@@ -68,17 +71,26 @@ def select_all(tablename: str):
     print(f"printing {tablename}")
     for row in results:
         print(row)
+        
+        
+def select_all_tables():
+    for tbl in ("actors", "movies", "movie_actors"):
+        select_all(tbl)
+        
+        
+
+        
 
 def test():
     names = [
-        'Tim Robbins', 'Morgan Freeman', 'Bob Gunton', 'William Sadler', 'Clancy Brown',
+        'Tim Robbins', "Morgan d'Freeman", 'Bob Gunton', 'William Sadler', 'Clancy Brown',
         'Gil Bellows', 'Mark Rolston', 'James Whitmore', 'Jeffrey DeMunn', 'Larry Brandenburg',
         'Neil Giuntoli', 'Brian Libby', 'David Proval', 'Joseph Ragno', 'Jude Ciccolella',
         'Paul McCrane', 'Renee Blaine', 'Scott Mann'
     ]
     insert_movie(
         title="Die Verurteilten", year=1994, genre="Drama", rating=9.2,
-        actor_ages=[30 for x in names], actor_names=names
+        actor_ages=[30+idx for idx, x in enumerate(names)], actor_names=names
     )
     [select_all(tbl) for tbl in ("actors", "movies", "movie_actors")]
     
@@ -90,7 +102,7 @@ def test():
     ]
     insert_movie(
         title="Die Verurteilten2", year=1995, genre="Dramacomedy", rating=1.3,
-        actor_ages=[30 for x in names], actor_names=names
+        actor_ages=[30+idx for idx, x in enumerate(names)], actor_names=names
     )
     [select_all(tbl) for tbl in ("actors", "movies", "movie_actors")]
 
